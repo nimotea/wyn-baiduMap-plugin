4447cb4e5a4479d0db6254b8b673de6e3e77d275
Hannes.Han <hannes.han@grapecity.com>
2024-01-31 12:19:38 +0800

    update code

--- src/visual.ts
+++ src/visual.ts
@@ -6,8 +6,18 @@ import { gcoordType } from "./coordinates";
 import { OPTIONKEY } from "./optionKey";
 import "bmapgllibs";
 // import "./AreaRestriction_min.js";
-import { debounce, debounceTime, fromEvent, tap } from "rxjs";
-import markerImage from '../assets/marker_red.png';
+import {
+  debounce,
+  debounceTime,
+  filter,
+  fromEvent,
+  map,
+  merge,
+  switchMap,
+  take,
+  tap,
+} from "rxjs";
+import markerImage from "../assets/marker_red.png";
 
 export default class Visual extends WynVisual {
   private static root: Visual;
@@ -29,14 +39,14 @@ export default class Visual extends WynVisual {
   private minLat: number = 999;
   private maxLng: number = -999;
   private maxLat: number = -999;
-  private HugeIcon : BMapGL.Icon = new BMapGL.Icon(
+  private HugeIcon: BMapGL.Icon = new BMapGL.Icon(
     markerImage,
-    new BMapGL.Size(30,30)
-  )
-  private NormalIcon : BMapGL.Icon = new BMapGL.Icon(
+    new BMapGL.Size(30, 30)
+  );
+  private NormalIcon: BMapGL.Icon = new BMapGL.Icon(
     markerImage,
-    new BMapGL.Size(23,25)
-  )
+    new BMapGL.Size(23, 25)
+  );
 
   constructor(
     dom: HTMLDivElement,
@@ -45,6 +55,9 @@ export default class Visual extends WynVisual {
   ) {
     super(dom, host, options);
     Visual.root = this;
+    window.parent.oncontextmenu=function(e){    //取消默认的浏览器自带右键 很重要！！    
+      e.preventDefault();
+    }
 
     this.dom = dom;
     this.host = host;
@@ -67,11 +80,11 @@ export default class Visual extends WynVisual {
     var zoomCtrl = new BMapGL.ZoomControl();
     this.map.addControl(scaleCtrl);
     this.map.addControl(zoomCtrl);
-    fromEvent(this.map, "click").subscribe((pointer) => {
+    fromEvent(this.map, "mousedown").subscribe((pointer) => {
       this.selectionManager.clear();
-      this.markers.forEach(marker => {
+      this.markers.forEach((marker) => {
         marker.setIcon(this.NormalIcon);
-    })
+      });
     });
   }
 
@@ -101,8 +114,10 @@ export default class Visual extends WynVisual {
         this.minLat = point[1];
       }
       let mapPoint = new BMapGL.Point(point[0], point[1]);
-      
-      let marker = new BMapGL.Marker(mapPoint);
+
+      let marker = new BMapGL.Marker(mapPoint, {
+        icon: this.NormalIcon,
+      });
 
       this.points.push(mapPoint);
       this.markers.push(marker);
@@ -130,26 +145,47 @@ export default class Visual extends WynVisual {
           this.host.toolTipService.hide();
         });
 
-      fromEvent(marker, "click")
-        .pipe( tap((e)=> { e.domEvent.stopPropagation();}))
-        .subscribe((pointer) => {
-          this.markers.forEach(marker => {
+      let mouseDown$ = fromEvent(marker, "mousedown");
+      mouseDown$
+        .pipe(
+          tap(e => {e.domEvent.stopPropagation();}),
+          map((event) => ({
+            domEvent: event.domEvent,
+            button: event.domEvent.button,
+            type: event.domEvent.type,
+          })),
+          filter((event) => event.button === 0), //左键处理逻辑
+          tap((e) => {
+            e.domEvent.stopPropagation();
+            this.markers.forEach((marker) => {
               marker.setIcon(this.NormalIcon);
-          })
-          marker.setIcon(this.HugeIcon);
-          this.selectionManager.clear();
-          // this.host.toolTipService.hide();
-          this.host.contextMenuService.hide();
-
-          const selectionId = this.host.selectionService.createSelectionId();
-          //keys
-          this.plainDataView.profile.tooltip.values.map((pro) => {
-            selectionId.withMeasure(pro, item);
+            });
+            marker.setIcon(this.HugeIcon);
+            this.selectionManager.clear();
+            this.host.contextMenuService.hide();
+            const selectionId = this.host.selectionService.createSelectionId();
+            this.plainDataView.profile.tooltip.values.map((pro) => {
+              selectionId.withMeasure(pro, item);
+            });
+            this.plainDataView.profile.category.values.map((pro) => {
+              selectionId.withDimension(pro, item);
+            });
+            this.selectionManager.select(selectionId, true);
+          }),
+          switchMap(() =>
+          mouseDown$.pipe(
+              filter((event) => event.domEvent.button === 2),
+              take(1)
+            )
+          )
+        )
+        .subscribe((event) => { // 右键处理逻辑
+          this.host.contextMenuService.show({
+            position: {
+              x: event.clientX,
+              y: event.clientY,
+            },
           });
-          this.plainDataView.profile.category.values.map((pro) => {
-            selectionId.withDimension(pro, item);
-          });
-          this.selectionManager.select(selectionId, true);
         });
     });
   }
@@ -193,17 +229,18 @@ export default class Visual extends WynVisual {
     this.addMonitorArea();
   }
 
-  private addMonitorArea(){
+  private addMonitorArea() {
     this.monitorMarkers = [];
     this.circles = [];
-    this.monitorMarkers = 
-    this.properties[OPTIONKEY.CUSTOME_POINT].map(monitor =>{
-      let monitorMarker:BMapGL.Marker;
-      if(monitor[OPTIONKEY.CUSTOME_LAT] &&
-        monitor[OPTIONKEY.CUSTOME_LNG] &&
-        monitor[OPTIONKEY.CUSTOME_COORDINATES] &&
-        monitor[OPTIONKEY.CUSTOME_IMAGE] 
-        ){
+    this.monitorMarkers = this.properties[OPTIONKEY.CUSTOME_POINT].map(
+      (monitor) => {
+        let monitorMarker: BMapGL.Marker;
+        if (
+          monitor[OPTIONKEY.CUSTOME_LAT] &&
+          monitor[OPTIONKEY.CUSTOME_LNG] &&
+          monitor[OPTIONKEY.CUSTOME_COORDINATES] &&
+          monitor[OPTIONKEY.CUSTOME_IMAGE]
+        ) {
           let point = gcoord.transform(
             [monitor[OPTIONKEY.CUSTOME_LNG], monitor[OPTIONKEY.CUSTOME_LAT]],
             gcoordType(monitor[OPTIONKEY.CUSTOME_COORDINATES]),
@@ -211,25 +248,33 @@ export default class Visual extends WynVisual {
           );
           let mapPoint = new BMapGL.Point(point[0], point[1]);
           monitorMarker = new BMapGL.Marker(mapPoint);
-          let Icon = new BMapGL.Icon(monitor[OPTIONKEY.CUSTOME_IMAGE],
-            new BMapGL.Size(50,50))
+          let Icon = new BMapGL.Icon(
+            monitor[OPTIONKEY.CUSTOME_IMAGE],
+            new BMapGL.Size(50, 50)
+          );
           monitorMarker.setIcon(Icon);
           // make a monitor point label
           var opts = {
             width: 100,
             height: 100,
-            title: monitor[OPTIONKEY.CUSTOME_POINT_NAME]
-        };
-        var infoWindow = new BMapGL.InfoWindow('', opts);
-          fromEvent(monitorMarker,"click")
-          .pipe(
-            tap(e => { e.domEvent.stopPropagation();})
-          ).subscribe((pointer)=>{
+            title: monitor[OPTIONKEY.CUSTOME_POINT_NAME],
+          };
+          var infoWindow = new BMapGL.InfoWindow("", opts);
+          fromEvent(monitorMarker, "click")
+            .pipe(
+              tap((e) => {
+                e.domEvent.stopPropagation();
+              })
+            )
+            .subscribe((pointer) => {
               this.map.openInfoWindow(infoWindow, mapPoint); // 开启信息窗口
-          });
+            });
 
-          let circle = new BMapGL.Circle(mapPoint,monitor[OPTIONKEY.CUSTOM_RADIUS]);
-          if(this.CalculateWarning(circle)){
+          let circle = new BMapGL.Circle(
+            mapPoint,
+            monitor[OPTIONKEY.CUSTOM_RADIUS]
+          );
+          if (this.CalculateWarning(circle)) {
             circle.setFillColor(monitor[OPTIONKEY.CUSTOM_WARN_COLOR]);
             circle.setStrokeColor(monitor[OPTIONKEY.CUSTOM_WARN_COLOR]);
           } else {
@@ -239,28 +284,28 @@ export default class Visual extends WynVisual {
           this.circles.push(circle);
         }
         return monitorMarker;
-    })
-    this.monitorMarkers.map(marker => {
+      }
+    );
+    this.monitorMarkers.map((marker) => {
       this.map.addOverlay(marker);
-    })
-    this.circles.map(circle => {
+    });
+    this.circles.map((circle) => {
       this.map.addOverlay(circle);
-    })
+    });
   }
 
-  private CalculateWarning(circle:BMapGL.Circle){
-      let isWarnStatus = false;
-      this.points.some(point => {
-        if(this.map.getDistance(
-          circle.getCenter(),
-          point
-        )<= circle.getRadius()){
-          isWarnStatus = true;
-          return true;
-        }
-        return false;
-      })
-      return isWarnStatus;
+  private CalculateWarning(circle: BMapGL.Circle) {
+    let isWarnStatus = false;
+    this.points.some((point) => {
+      if (
+        this.map.getDistance(circle.getCenter(), point) <= circle.getRadius()
+      ) {
+        isWarnStatus = true;
+        return true;
+      }
+      return false;
+    });
+    return isWarnStatus;
   }
 
   public onDestroy(): void {}
